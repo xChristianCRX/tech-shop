d413b16d2343c601402760a3aaaaadc16d0453a4
Christian Ricci <riccichristian338@gmail.com>
2025-03-05 18:14:56 -0300

    Feat: Button for insert a new product

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,17 @@
-import logoImg from "../../assets/logo-techshop.svg";
 import { Button } from "../ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "../ui/dialog";
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
+
+import logoImg from "../../assets/logo-techshop.svg";
 import styles from "./header.module.css";
 
 export function Header() {
@@ -7,9 +19,46 @@ export function Header() {
     <header className="border-b">
       <div className="flex h-16 items-center gap-6 px-6">
         <img src={logoImg} alt="Logo do site TechShop" width={200} />
-        
+
         <div className="ml-auto flex items-center gap-2">
-          <Button>Adicionar Produto</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Cadastrar Produto</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Cadastro de novo produto</DialogTitle>
+                <DialogDescription>
+                  Insira as informações do novo produto.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Name
+                  </Label>
+                  <Input
+                    id="name"
+                    value="Pedro Duarte"
+                    className="col-span-3"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="username" className="text-right">
+                    Username
+                  </Label>
+                  <Input
+                    id="username"
+                    value="@peduarte"
+                    className="col-span-3"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </header>
